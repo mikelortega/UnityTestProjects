ab22709008b58e7af47f4329acea9692f15c9d9b
Mikel Ortega <mikelo@gmail.com>
2015-03-03 22:52:28 +0100

    Checking Unity 5.0.0f4 particles ...Max particle size problem with Stretched billboards persists.

---
+++ Spheres/Assets/CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour
+{
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.W))
+			transform.Translate(0.0f, 0.0f, 0.1f);
+
+		if (Input.GetKey(KeyCode.S))
+			transform.Translate(0.0f, 0.0f, -0.1f);
+
+		if (Input.GetKey(KeyCode.A))
+			transform.Translate(-0.1f, 0.0f, 0.1f);
+
+		if (Input.GetKey(KeyCode.D))
+			transform.Translate(0.1f, 0.0f, 0.1f);
+	}
+}
